da1e00f5b552a2349053f95149fe40c0ab16c1d8
huangjundashuaige <715817451@qq.com>
2018-10-12 00:38:23 +0800

    code all done

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
+
+[TOC]
+
 # selpg
-homework for service computing
+first thing first,i aint gonna lie,this whole project was designed just for homework in the course of service computing.
+
+i wont even learn golang if i aint took that course.but you know,everything just wont workout like we used to think.
+
+so lets go get them,first i am gonna intro some basic part about golang which used during the exper.then some tricky part about this problem.after that some conclusion.
+
+1. 
--- selpg.go
+++ selpg.go
@@ -7,12 +7,23 @@ import(
 	"os"
 	"errors"
 )
+/*
+
+set up for the pflag 
+
+*/
 
 var input_start_line = flag.IntP("start","s",1,"target paragraph's start line")
 var input_end_line = flag.IntP("end","e",1,"target paragraph's end line")
 var input_page_size = flag.IntP("line","l",72,"how many line one page contain")
 var error_writer = bufio.NewWriterSize(os.Stderr,1024)
 
+
+/*
+
+make sure the inputs are all legal
+
+*/
 func negative_input(i *int)(error){
 	if(*i < 0){
 		return errors.New("input cant be negative")
@@ -20,6 +31,11 @@ func negative_input(i *int)(error){
 	return nil
 }
 
+/*
+
+make sure the start position is definately small than end position
+
+*/
 func check_start_lower_end(start *int,end *int)(error){
 	if(*start > *end){
 		return errors.New("start is larger than end position")
@@ -27,6 +43,11 @@ func check_start_lower_end(start *int,end *int)(error){
 	return nil
 }
 
+/*
+
+whole checking process
+
+*/
 func check_legal_input(){
 	if err:=negative_input(input_start_line);err!=nil{
 		error_writer.WriteString(err.Error())
@@ -50,6 +71,11 @@ func check_legal_input(){
 	}
 }
 
+/*
+
+standard mechaism for handling the error
+
+*/
 func check_err(err error){
 	if err != nil{
 		error_writer.WriteString(err.Error())
@@ -58,26 +84,56 @@ func check_err(err error){
 }
 
 func main(){
-	flag.Parse()
+	flag.Parse() 
+	/*
+
+	dealing with the input, make sure which is which
+
+	*/
 	check_legal_input()
-	var f *bufio.Reader
+	
+
+	var f *bufio.Reader //creat the reader object
+	// to simplfy the read afterwards option
+	
+	
 	if(flag.NArg()!=0){
+	/*
+
+		not providing the input file address
+
+	*/
 		file,err := os.Open(flag.Arg(flag.NArg()-1))
+		// openning the file
 		check_err(err)
-		f = bufio.NewReader(file)	
+		f = bufio.NewReader(file)
+		//inititialize the object as the trash can	
+	
 	}else{
+		/*
+
+		normal procedure everything comes from stand input
+
+		*/
 		f = bufio.NewReader(os.Stdin)
 	}
 		var output_page string
+		//the overall result
 	for i:=0;;i++{
 		line,_ := f.ReadString('\n')
 		//check_err(err)
 		var s string
 		fmt.Sscan(line,&s)
+		//read the line one by one
 		if(len(s)==0){
 			break
 		}
 		if(i>=( *input_start_line-1)*(*input_page_size) && i<=(*input_end_line)*(*input_page_size)){
+			/*
+
+			combine all line into one large string
+
+			*/
 			output_page +="\n"
 			output_page +=s
 
